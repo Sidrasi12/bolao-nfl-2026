72f4284fc40534f1ff3044a915f7710acdd90572
Sidrasi12 <guilhermerasi@gmail.com>
2026-09-09 10:02:49 -0300

    Update deadline for preseason predictions

--- js/regulation.js
+++ js/regulation.js
@@ -54,7 +54,7 @@ Bolao.Regulation = {
           <li>equipe de pior campanha;</li>
           <li>MVP da temporada.</li>
         </ul>
-        <p>7.2. Os palpites deverão ser registrados até as 23h59 do dia 8 de setembro de 2026, no horário de Brasília, e não poderão ser alterados depois do encerramento.</p>
+        <p>7.2. Os palpites de pré-temporada poderão ser preenchidos e alterados até as 23h59 do dia 11 de setembro de 2026, no horário de Brasília. A partir das 00h00 do dia 12 de setembro de 2026, não serão permitidas novas gravações ou alterações.</p>
         <p>7.3. A ausência de palpite em qualquer item valerá zero ponto.</p>
 
         <h2>8. Pontuação dos palpites de pré-temporada</h2>
